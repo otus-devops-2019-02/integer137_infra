9a9e880faac7aac0ac34f54b269e888552dd0878
Pavel Tegletsov <integer137@gmail.com>
2019-04-20 22:09:29 +0300

    Terraform-2 with *

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,66 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+}
+  provisioner "file" {
+    source      = "../modules/app/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "../modules/app/deploy.sh"
+}
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sed -i 's/#Environment/Environment=DATABASE_URL=${var.db_internal_ip}/g' /etc/systemd/system/puma.service",
+    ]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl daemon-reload",
+      "sudo systemctl restart puma.service"
+    ]
+  }
+}
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,7 @@
-variable project {
-  description = "Project ID"
+variable "project" {
+  description = "The project in which the resource belongs. If it is not provided, the provider project is used."
 }
-
-variable region {
-  description = "Region"
-
-  # Значение по умолчанию
-  default = "europe-west1"
-}
-variable count {
-  description = "count"
-
-  # Значение по умолчанию
-  default = "1"
-}
-
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
-}
-
-variable private_key_path {
-  description = "Path to the private key used for ssh access"
-}
-
-variable zone {
-  description = "Zone"
-
-  # Значение по умолчанию
-  default = "europe-west1-b"
-}
-
-variable disk_image {
-  description = "Disk image"
+variable "region" {
+  description = "The GCS region."
+  default     = "europe-west1"
 }
